f426592f42137e93bd32cd6a781b896f4fe1e4e0
码上有钱 <80383692+pan-guogui@users.noreply.github.com>
2022-11-06 15:52:35 +0800

    Add files via upload

---
+++ SAUtest/src/helloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author PanGg
+ * @date 2022年11月06日 14:32
+ */
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
