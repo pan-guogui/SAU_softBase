c2bfa32e9f80f99bd8168beedb17d1455b485c9d
码上有钱 <80383692+pan-guogui@users.noreply.github.com>
2022-11-08 15:31:21 +0800

    Add files via upload

---
+++ SAUtest/src/test.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author PanGg
+ * @date 2022年11月07日 16:35
+ */
+
+
+public class test {
+
+    int[] block = {1, 2, -3, 2};
+
+    @Test
+
+    public void TestMax() {
+
+        assertEquals(3, maxArray.Max(block));
+
+    }
+
+}
